97e9056f2f5e2d4f9d2bd6dc39b21270595e8f94
XiaJun <XiaJun@XiaJundeMacBook-Air.local>
2016-05-31 17:27:51 +0800

    Initial Commit

---
+++ github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  github
+//
+//  Created by XiaJun on 16/5/31.
+//  Copyright (c) 2016年 XiaJun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ github/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  github
+//
+//  Created by XiaJun on 16/5/31.
+//  Copyright (c) 2016年 XiaJun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
